2250dc16446ce68266a7c2836f8796d41dd35cdf
NathanLCR <nathan.rocha@maisunifacisa.com.br>
2020-06-03 09:49:10 -0300

    Mobile added

---
+++ Mobile/src/pages/SignIn/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding: 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-medium';
+  margin: 64px 0 24px;
+`;
